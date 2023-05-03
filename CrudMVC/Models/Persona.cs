2ceda8a431a1f73f473274ed2b5d3f297354e379
Alvaro <Alvaro@DESKTOP-S4GBVSP>
2023-05-03 18:55:27 -0500

    Agregar archivos de proyecto.

---
+++ CrudMVC/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudMVC.Models;
+
+public partial class Persona
+{
+    public int Id { get; set; }
+
+    public string Nombres { get; set; } = null!;
+
+    public string Apellidos { get; set; } = null!;
+
+    public string Telefono { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
---
+++ CrudMVC/Models/TutorialCrudContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC.Models;
+
+public partial class TutorialCrudContext : DbContext
+{
+    public TutorialCrudContext()
+    {
+    }
+
+    public TutorialCrudContext(DbContextOptions<TutorialCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellidos)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("apellidos");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombres)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombres");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
